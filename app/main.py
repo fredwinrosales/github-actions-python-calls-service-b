76f221e77d88f88bed13b09268e74da39bd27b73
Fredwin Rosales <fredwinrosales@gmail.com>
2025-03-25 21:00:55 +0000

    Configurar comuncicación con servicio b

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI!"}
+TARGET_URL = "http://10.43.127.150"
+
+@app.get("/comunicar")
+def comunicar_con_typescript():
+    try:
+        respuesta = httpx.get(TARGET_URL)
+        return {
+            "mensaje_recibido": respuesta.json(),
+            "origen": "Microservicio A (Python)"
+        }
+    except Exception as e:
+        return {"error": str(e)}
